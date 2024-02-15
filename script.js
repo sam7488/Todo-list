5a93d944c4fd3562c6033a05950a6709fb00e9c0
Sumit Sah <ssah48731@gmail.com>
2024-02-15 22:32:35 +0545

    update checkmark

--- script.js
+++ script.js
@@ -4,20 +4,7 @@ renderTodoList();
 
 document.querySelector('.js-add-todo-button').addEventListener('click', addTodo );
 
-const checkbox = document.querySelectorAll('.js-checkbox');
-checkbox.forEach(
-  (checkItem) => {
-    checkItem.addEventListener(
-      'change', 
-      () => {
-        if(checkItem.checked) {
-
-        }
-
-      }
-    )
-  }
-)
+document.querySelectorAll('.js-')
 
 function addTodo() {
   const inputElement = document.querySelector('.js-input-name');
@@ -41,7 +28,7 @@ function addTodo() {
   });
   localStorage.setItem('todolist', JSON.stringify(todoList));
 
-  inputElement.value = '';
+  inputElement.value = 'abc';
   renderTodoList();
 }
 
@@ -51,7 +38,12 @@ function renderTodoList() {
     (todoObject, i) => {
       const {name, dueDate} = todoObject;
       todoListHTML += `
-      <label class="text" for="checkID-${i}">${name}</label></div>
+      <div class="todo">
+        <div class="js-checkmark checkmark" data-index="${i}">
+          <img src="unchecked-checkbox.png" alt="unchecked">
+        </div>
+        <div class="js-text-${i} text">${name}</div>
+      </div>
       <div>${dueDate}</div>
       <button
       class="js-delete-todo-button delete-todo-button">
@@ -63,6 +55,22 @@ function renderTodoList() {
 
   const todoElement = document.querySelector('.js-todo-list');
   todoElement.innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-checkmark').forEach(
+    (checkbox, index) => {
+      checkbox.addEventListener(
+        'click',
+        () => {
+          document.querySelector(`.js-text-${index}`).classList.toggle('text-line-through')
+          if(document.querySelector(`.js-text-${index}`).classList.contains('text-line-through')) {
+            checkbox.innerHTML = `<img src="checked-checkbox.png" alt="checked">`;
+          } else {
+            checkbox.innerHTML = `<img src="unchecked-checkbox.png" alt="unchecked">`;
+          }
+        }
+      )
+    }
+  )
 
   document.querySelectorAll('.js-delete-todo-button').forEach(
     (deleteButton, index) => {
